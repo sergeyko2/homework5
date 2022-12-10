1112bcf726b11880815e7719f110688d92f244f9
sergeyko2 <sergeyko2@gmail.com>
2022-12-11 00:40:04 +0200

    begin code main

--- src/main/java/task5/Main.java
+++ src/main/java/task5/Main.java
@@ -5,13 +5,18 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        showQuestion();
 
-        
+
 
 
     }
 
+    private static void showQuestion() {
+        System.out.println();
 
+    }
 
 
 }
